6b41a4b868d1d2b0befd9dab6b3969382cd5967c
koyu <me@koyu.space>
2024-08-03 00:27:56 +0200

    Fix some encoding issues

--- plcheck.py
+++ plcheck.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import os
 
 fls = os.listdir()
@@ -5,12 +7,12 @@ fls = os.listdir()
 for fl in fls:
     if ".m3u" in fl:
         tmp = ""
-        with open(fl, "r") as f:
+        with open(fl, "r", encoding="utf8") as f:
             x = f.readlines()
             for line in x:
                 if not "/Music/" in line:
                     tmp += "/Music/"+line
                 else:
                     tmp += line
-        with open(fl, "w") as f:
+        with open(fl, "w", encoding="utf8") as f:
             f.write(tmp)
