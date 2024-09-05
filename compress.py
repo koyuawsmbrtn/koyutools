79ea8242675051ffc7ef7a3c61ea9ed0fbe51baa
koyu <me@koyu.space>
2024-09-05 15:36:47 +0200

    Add compress.py

---
+++ compress.py
@@ -0,0 +1,6 @@
+from glob import glob
+from subprocess import run
+
+for flac in glob("**/*.flac", recursive=True): 
+    mp3 = flac.replace(".flac", ".mp3") 
+    run(["ffmpeg", "-i", flac, "-c:v", "copy", "-b:a", "320k", mp3])
