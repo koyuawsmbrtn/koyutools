fb15fa47041bc266d5041812721e14d7bc548be8
koyu <me@koyu.space>
2025-03-21 04:42:15 +0100

    Fix some stuff with the playlister script

--- sisthatsrisky/playlister.py
+++ sisthatsrisky/playlister.py
@@ -1,37 +1,65 @@
 #!/usr/bin/python3
 
+import re
+import unicodedata
 import urllib.request
 import os
 from bs4 import BeautifulSoup
+from retry import retry
 
-arg = os.sys.argv[1]
-f = open(arg, "r")
-tracks = f.readlines()
-f.close()
-tracknum = len(tracks)
-currtrack = 0
+# Taken from https://github.com/django/django/blob/main/django/utils/text.py
+def slugify(value, allow_unicode=False):
+    """
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize("NFKC", value)
+    else:
+        value = (
+            unicodedata.normalize("NFKD", value)
+            .encode("ascii", "ignore")
+            .decode("ascii")
+        )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
 
-filenamewo = arg.split(".")[0]
+@retry(tries=3, delay=3)
+def main():
+    arg = os.sys.argv[1]
+    f = open(arg, "r")
+    tracks = f.readlines()
+    f.close()
+    tracknum = len(tracks)
+    currtrack = 0
 
-os.system("rm -f "+filenamewo+".m3u")
+    filenamewo = arg.split(".")[0]
 
-for surl in tracks:
-    currtrack = currtrack + 1
-    try:
-        trackid = surl.replace("https://open.spotify.com/track/", "").split("?")[0]
-    except:
-        trackid = surl.replace("https://open.spotify.com/track/", "")
-    print("Track " + str(currtrack) + " of " + str(tracknum) + " (" + trackid.split("\n")[0] + ")")
-    if not "http" in surl:
-        surl = "https://open.spotify.com/track/" + surl
-    print("Downloading...")
-    with urllib.request.urlopen(surl) as response:
-        r = response.read().decode()
-    soup = BeautifulSoup(r, "lxml")
-    artist = soup.find("meta", {"name": "music:musician_description"})["content"]
-    title = soup.find("meta", {"property": "og:title"})["content"]
-    filename = artist.replace(" ", "-").replace("/", "-") + "_" + title.replace(" ", "-").replace("/", "-") + ".mp3"
-    if os.path.exists(filename):
-        f = open(filenamewo+".m3u", "a+", encoding="utf-8")
-        f.write(filename + "\n")
-        f.close()
+    os.system("rm -f "+filenamewo+".m3u")
+    for surl in tracks:
+        currtrack = currtrack + 1
+        try:
+            trackid = surl.replace("https://open.spotify.com/track/", "").split("?")[0]
+        except:
+            trackid = surl.replace("https://open.spotify.com/track/", "")
+        print("Track " + str(currtrack) + " of " + str(tracknum) + " (" + trackid.split("\n")[0] + ")")
+        if not "http" in surl:
+            surl = "https://open.spotify.com/track/" + surl
+        print("Downloading...")
+        with urllib.request.urlopen(surl) as response:
+            r = response.read().decode()
+        soup = BeautifulSoup(r, "lxml")
+        artist = soup.find("meta", {"name": "music:musician_description"})["content"]
+        title = soup.find("meta", {"property": "og:title"})["content"]
+        filename = artist.replace(" ", "-").replace("/", "-") + "_" + title.replace(" ", "-").replace("/", "-")
+        filename = slugify(filename) + ".mp3"
+        if os.path.exists(filename):
+            f = open(filenamewo+".m3u", "a+", encoding="utf-8")
+            f.write(filename + "\n")
+            f.close()
+
+if __name__ == "__main__":
+    main()
